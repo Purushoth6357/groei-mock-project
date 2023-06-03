e11a559dc274bdfda489de13b126ccd0bd85e10e
Purushoth6357 <108821123+Purushoth6357@users.noreply.github.com>
2023-06-03 11:50:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const sideMenu = document.getElementById("sidemenu");
+
+
+function openMenu() {
+    sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+    sideMenu.style.right = "-200px";
+}
